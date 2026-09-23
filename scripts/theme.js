a3429b429f758fd5d1e49b64eb15b680ff4c0aff
nastya3323 <gomes-96@mail.ru>
2026-09-23 19:47:38 +0400

    fix: prevent fouc for saved theme

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,10 +1,5 @@
 export default function initTheme() {
   const root = document.documentElement;
-  const savedTheme = localStorage.getItem("theme");
-
-  if (savedTheme) {
-    root.dataset.theme = savedTheme;
-  }
 
   const themeToggleButton = document.querySelector(".theme-switch");
 
@@ -15,6 +10,9 @@ export default function initTheme() {
   themeToggleButton.addEventListener("click", () => {
     const theme = root.dataset.theme === "light" ? "dark" : "light";
     root.dataset.theme = theme;
-    localStorage.setItem("theme", theme);
+
+    try {
+      localStorage.setItem("theme", theme);
+    } catch {}
   });
 }
